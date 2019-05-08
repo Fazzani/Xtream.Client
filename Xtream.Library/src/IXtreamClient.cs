1da9f43f2c9974016a750415f69d2f0072fab8ef
Heni Fazzani <heni.fazzani@seloger.com>
2019-05-08 16:51:15 +0200

    fixes

--- Xtream.Library/src/IXtreamClient.cs
+++ Xtream.Library/src/IXtreamClient.cs
@@ -6,14 +6,14 @@ namespace Xtream.Client
 {
     public interface IXtreamClient
     {
-        Task<List<Epg_Listings>> GetAllEpgAsync(CancellationToken cancellationToken);
-        Task<List<Live>> GetLiveCategoriesAsync(CancellationToken cancellationToken);
-        Task<List<Channels>> GetLiveStreamsAsync(CancellationToken cancellationToken);
-        Task<List<Channels>> GetLiveStreamsByCategoriesAsync(string categoryId, CancellationToken cancellationToken);
-        Task<XtreamPanel> GetPanelAsync(CancellationToken cancellationToken);
-        Task<List<Epg_Listings>> GetShortEpgForStreamAsync(string streamId, CancellationToken cancellationToken);
-        Task<PlayerApi> GetUserAndServerInfoAsync(CancellationToken cancellationToken);
-        Task<List<Channels>> GetVodStreamsAsync(CancellationToken cancellationToken);
-        Task<PlayerApi> GetXmltvAsync(CancellationToken cancellationToken);
+        Task<List<Epg_Listings>> GetAllEpgAsync(CancellationToken cancellationToken = default);
+        Task<List<Live>> GetLiveCategoriesAsync(CancellationToken cancellationToken = default);
+        Task<List<Channels>> GetLiveStreamsAsync(CancellationToken cancellationToken = default);
+        Task<List<Channels>> GetLiveStreamsByCategoriesAsync(string categoryId, CancellationToken cancellationToken = default);
+        Task<XtreamPanel> GetPanelAsync(CancellationToken cancellationToken = default);
+        Task<List<Epg_Listings>> GetShortEpgForStreamAsync(string streamId, CancellationToken cancellationToken = default);
+        Task<PlayerApi> GetUserAndServerInfoAsync(CancellationToken cancellationToken = default);
+        Task<List<Channels>> GetVodStreamsAsync(CancellationToken cancellationToken = default);
+        Task<PlayerApi> GetXmltvAsync(CancellationToken cancellationToken = default);
     }
 }
